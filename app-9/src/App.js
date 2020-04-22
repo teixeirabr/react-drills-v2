f4342da15c1268722745245f2c68bf26fbd80e7e
andre <andrelaprov@gmail.com>
2020-04-22 15:42:49 -0400

    HashRouter on index.js

--- app-9/src/App.js
+++ app-9/src/App.js
@@ -1,23 +1,22 @@
 import React, { Component } from "react";
 import "./App.css";
-import { HashRouter, Link } from "react-router-dom";
+// import { HashRouter, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import router from "./router";
 
 export default class App extends Component {
   render() {
     return (
-      <HashRouter>
-        <div className="App">
-          <nav>
-            <ul>
-              <Link to="/">Home</Link>
-              <Link to="./signup">Signup</Link>
-              <Link to="./details">Details</Link>
-            </ul>
-          </nav>
-          {router}
-        </div>
-      </HashRouter>
+      <div className="App">
+        <nav>
+          <ul>
+            <Link to="/">Home</Link>
+            <Link to="./signup">Signup</Link>
+            <Link to="./details">Details</Link>
+          </ul>
+        </nav>
+        {router}
+      </div>
     );
   }
 }
